d5768fc2cd6ff775f9544cf6a453d6613ab6ee66
MatthewDWise <68114189+MatthewDWise@users.noreply.github.com>
2020-08-20 10:22:52 -0400

    Update script.js

--- script.js
+++ script.js
@@ -4,8 +4,7 @@ function getDogImage(numberInput) {
     if (!numberInput){
         fetch("https://dog.ceo/api/breeds/image/random/3")
         .then(response => response.json())
-        .then(responseJson => displayResults(responseJson))
-        .catch(error => alert('Something went wrong.  Try again later.'));        
+        .then(responseJson => displayResults(responseJson));        
     } else if (numberInput > 50) {
         return alert ("50 is the maximum number of pups, please choose again!");
     } else if (numberInput == 0) {
